dec967ffbf689823294ab7a20d157831f8bc4791
mench0 <46263998+mench0@users.noreply.github.com>
2021-01-27 21:16:11 +0300

    2.1.4

--- src/main/java/koschei/models/Wood3.java
+++ src/main/java/koschei/models/Wood3.java
@@ -1,12 +1,12 @@
 package koschei.models;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Component;
 
 @Component
 public class Wood3 {
-
-    private final Rabbit4 rabbit;
+    private Rabbit4 rabbit;
 
     @Autowired
     public Wood3(Rabbit4 rabbit) {
